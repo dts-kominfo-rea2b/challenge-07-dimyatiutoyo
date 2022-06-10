4bac9d2c44510067970c44ddecf51c0c84b090b7
Dimyati Utoyo <dimyatiutoyo.b@gmail.com>
2022-06-10 15:47:44 +0700

    menambah data untuk menuju batas dan melampauinya

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let count = 0;
+
+  // Karena berbentuk promise,
+  // tunggu promiseTheaterIXX hingga selesai
+  // menggunakan await.
+  // berlaku juga untuk promiseTheaterVGC
+  let ixx = await promiseTheaterIXX();
+  ixx.forEach(element => {
+    if (element.hasil == emosi) {
+      count += 1;
+    }
+  });
+
+  let vgc = await promiseTheaterVGC();
+  vgc.forEach(element => {
+    if (element.hasil == emosi) {
+      count += 1;
+    }
+  });
+
+  return count;
+};
 
 module.exports = {
   promiseOutput,
